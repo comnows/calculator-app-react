6b3867c03b87a4b0171c7eeec271befba541556a
comnows <watthanandh.l@mail.kmutt.ac.th>
2024-02-08 13:56:02 +0700

    Add buttons design and animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,74 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="grid grid-cols-[540px] justify-center items-center gap-y-6 mt-24">
+        <div className="header">
+          <div className="title text-current-num-first">calc</div>
+          <div className="toggle-button"></div>
+        </div>
+        <div className="output bg-screen-first px-8 py-6 w-full text-right rounded-lg">
+          <div className="previous-operand"></div>
+          <div className="current-operand text-current-num-first">399,981</div>
+        </div>
+        <div className="input-grid grid grid-cols-4 gap-x-6 gap-y-7 p-8 bg-keypad-first rounded-lg">
+          <button className="text-key-num-first bg-key-first hover:bg-key-hover-first rounded-lg shadow-key-shadow-first pt-2 pb-1 transition active:translate-y-1 active:shadow-none">
+            7
+          </button>
+          <button className="text-key-num-first bg-key-first hover:bg-key-hover-first rounded-lg shadow-key-shadow-first pt-2 pb-1 transition active:translate-y-1 active:shadow-none">
+            8
+          </button>
+          <button className="text-key-num-first bg-key-first hover:bg-key-hover-first rounded-lg shadow-key-shadow-first pt-2 pb-1 transition active:translate-y-1 active:shadow-none">
+            9
+          </button>
+          <button className="text-current-num-first bg-delete-key-first hover:bg-delete-key-hover-first rounded-lg shadow-delete-key-shadow-first pt-2 pb-1 transition active:translate-y-1 active:shadow-none">
+            DEL
+          </button>
+          <button className="text-key-num-first bg-key-first hover:bg-key-hover-first rounded-lg shadow-key-shadow-first pt-2 pb-1 transition active:translate-y-1 active:shadow-none">
+            4
+          </button>
+          <button className="text-key-num-first bg-key-first hover:bg-key-hover-first rounded-lg shadow-key-shadow-first pt-2 pb-1 transition active:translate-y-1 active:shadow-none">
+            5
+          </button>
+          <button className="text-key-num-first bg-key-first hover:bg-key-hover-first rounded-lg shadow-key-shadow-first pt-2 pb-1 transition active:translate-y-1 active:shadow-none">
+            6
+          </button>
+          <button className="text-key-num-first bg-key-first hover:bg-key-hover-first rounded-lg shadow-key-shadow-first pt-2 pb-1 transition active:translate-y-1 active:shadow-none">
+            +
+          </button>
+          <button className="text-key-num-first bg-key-first hover:bg-key-hover-first rounded-lg shadow-key-shadow-first pt-2 pb-1 transition active:translate-y-1 active:shadow-none">
+            1
+          </button>
+          <button className="text-key-num-first bg-key-first hover:bg-key-hover-first rounded-lg shadow-key-shadow-first pt-2 pb-1 transition active:translate-y-1 active:shadow-none">
+            2
+          </button>
+          <button className="text-key-num-first bg-key-first hover:bg-key-hover-first rounded-lg shadow-key-shadow-first pt-2 pb-1 transition active:translate-y-1 active:shadow-none">
+            3
+          </button>
+          <button className="text-key-num-first bg-key-first hover:bg-key-hover-first rounded-lg shadow-key-shadow-first pt-2 pb-1 transition active:translate-y-1 active:shadow-none">
+            -
+          </button>
+          <button className="text-key-num-first bg-key-first hover:bg-key-hover-first rounded-lg shadow-key-shadow-first pt-2 pb-1 transition active:translate-y-1 active:shadow-none">
+            .
+          </button>
+          <button className="text-key-num-first bg-key-first hover:bg-key-hover-first rounded-lg shadow-key-shadow-first pt-2 pb-1 transition active:translate-y-1 active:shadow-none">
+            0
+          </button>
+          <button className="text-key-num-first bg-key-first hover:bg-key-hover-first rounded-lg shadow-key-shadow-first pt-2 pb-1 transition active:translate-y-1 active:shadow-none">
+            /
+          </button>
+          <button className="text-key-num-first bg-key-first hover:bg-key-hover-first rounded-lg shadow-key-shadow-first pt-2 pb-1 transition active:translate-y-1 active:shadow-none">
+            x
+          </button>
+          <button className="col-span-2 text-current-num-first bg-delete-key-first hover:bg-delete-key-hover-first rounded-lg shadow-delete-key-shadow-first pt-2 pb-1 transition active:translate-y-1 active:shadow-none">
+            RESET
+          </button>
+          <button className="col-span-2 text-current-num-first bg-toggle-first hover:bg-toggle-hover-first rounded-lg shadow-result-key-shadow-first pt-2 pb-1 transition active:translate-y-1 active:shadow-none">
+            =
+          </button>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,32 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    colors: {
+      'main-first': 'hsl(var(--color-background-main-first))',
+      'screen-first': 'hsl(var(--color-background-screen-first))',
+      'keypad-first': 'hsl(var(--color-background-keypad-first))',
+
+      'delete-key-first': 'hsl(var(--color-background-delete-key-first))',
+      'delete-key-hover-first': '#a2b2e3',
+      'delete-key-shadow-first': 'hsl(var(--color-shadow-delete-key-first))',
+
+      'toggle-first': 'hsl(var(--color-toggle-first))',
+      'toggle-hover-first': '#f96c5b',
+      'result-key-shadow-first': 'hsl(var(--color-shadow-result-key-first))',
+
+      'key-first': 'hsl(var(--color-background-key-first))',
+      'key-hover-first': '#ffffff',
+      'key-shadow-first': 'hsl(var(--color-shadow-key-first))',
+
+      'key-num-first': 'hsl(var(--color-key-first))',
+      'current-num-first': 'hsl(var(--color-num-first))',
+    },
+    boxShadow: {
+      'key-shadow-first': '0px 4px key-shadow-first',
+      'delete-key-shadow-first': '0px 4px delete-key-shadow-first',
+      'result-key-shadow-first': '0px 4px result-key-shadow-first',
+      'none': '0 0 #000000'
+    },
     extend: {},
   },
   plugins: [],
